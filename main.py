d85180d2e3c124e1c2cb27fdc2e290a1859d0c93
jcvd3v <jcvd3v@gmail.com>
2026-03-03 18:48:28 +0100

    graph

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
 import pandas as pd
-df = pd.read_csv("data\sales.csv", encoding="utf-8", header=0, index_col=0)
+df = pd.read_csv("data\sales.csv", encoding="utf-8", header=0 )
 
-print(df.head())
+print(df.columns)
